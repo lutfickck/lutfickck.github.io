d7aaba758ab8e0fceb46bcba1ff35282b8a96bb4
lutfickck <acdc4339@gmail.com>
2019-09-25 14:13:43 +0700

    commit satu

---
+++ js/myproduct.js
@@ -0,0 +1,28 @@
+vue.component('myproduct', {
+    template:'    
+        <div class= "container new-arrival" >
+            <div class="row">
+                <div class="col">
+                    <h5 class="title">NEW ARRIVAL</h5>
+                </div>
+            </div>
+            <div class="row">
+
+                <div class="item col-3">
+                    <img src="product.imgpath" alt="">
+                        <p class="product-name">Tees Renz Xray Stripped White</p>
+                        <p class="price">RP.160.000</p>
+                </div>
+        </div>
+        ,
+        data: function() {
+        return {
+                    productitem: [
+                {
+                    name:
+                }
+                    
+            ]
+        }'
+    }
+})
